28a3b762ff44c2b2f1680e0e2298e96750c74fa5
NalinTiwary <nalintiwary2409@gmail.com>
2022-03-07 23:32:44 -0600

    Partially Working CSS homepage

--- Django/Homepage/urls.py
+++ Django/Homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Assigning a Reference to a function to a URL
 urlpatterns = [
     path('', views.homepage),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
