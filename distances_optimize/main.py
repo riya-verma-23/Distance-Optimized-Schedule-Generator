fbeaff89ae31451ddb0f5d995af2355ab448c008
riya-verma-23 <riyarverma23@gmail.com>
2022-03-19 11:14:29 -0500

    added distance_optimize

---
+++ distances_optimize/main.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from array import *
+import distances_optimize.distances as distances
+from distances_optimize.distances import *
+
+# #API KEY
+api_key = "INSERT API KEY"
+
+# #input all locations
+origins = []
+destinations = []
+
+# #Asking User for Local Address Input 
+loc1 = input("Enter loc1 address: ")
+loc2 = input("Enter loc2 address: ")
+loc3 = input("Enter loc3 address: ")
+loc4 = input("Enter loc4 address: ")
+
+# #origins and destinations are the same for ditance between every location with each other
+origins.append(loc1)
+origins.append(loc2)
+origins.append(loc3)
+origins.append(loc4)
+destinations.append(loc1)
+destinations.append(loc2)
+destinations.append(loc3)
+destinations.append(loc4)
+
+
+# #function that generates the output file for the distance matrix as a JSON
+r = distances.Distance.distance_matrix(origins, destinations)
+
+# #function that parses through that JSON file and stores the distances between each as a 2D matrix
+matrix = distances.Distance.generateMatrixfromJSON(r, len(origins), len(destinations))
+
+# #Print out the matrix of distances in strings returned
+print(matrix)
+
+schedules = generateScheduleCombinations([[1,2], [3,4], [5, 6, 7]])
+print(schedules)
+
---
+++ distances_optimize/my_tests.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy as np
+import distances
+from distances import *
+
+# def test_distance_matrix():
+#     locations = ["chicago", "hendrick house", "new york city", "san francisco"]
+#     r = distances.Distance.distance_matrix(locations, locations)
+#     matrix = distances.Distance.generateMatrixfromJSON(r, len(locations), len(locations))
+#     ans = [['1 m', '222 km', '1,270 km', '3,424 km'], ['223 km', '1 m', '1,335 km', '3,460 km'], ['1,271 km', '1,331 km', '1 m', '4,670 km'], ['3,431 km', '3,466 km', '4,677 km', '1 m']]
+#     np.testing.assert_array_equal(matrix, ans)
+
+def test_generateScheduleCombinations():
+    ans = [[1, 3, 5], [1, 3, 6], [1, 3, 7], [1, 4, 5], [1, 4, 6], [1, 4, 7], [2, 3, 5], [2, 3, 6], [2, 3, 7], [2, 4, 5], [2, 4, 6], [2, 4, 7]]
+    schedules = distances.Distance.generateScheduleCombinations([[1,2], [3,4], [5, 6, 7]])
+    np.testing.assert_array_equal(ans, schedules)
+
+def test_eliminate_sections():
+    ans = [(1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 3), (2, 4), (2, 5), (2, 6), (3, 4), (3, 5), (3, 6), (4, 5), (4, 6), (5, 6)] 
+    eliminate = distances.Distance.generate_tuple_sections([1,2,3,4,5,6])
+    np.testing.assert_array_equal(ans, eliminate)
+    ans = [(1, 2), (1, 3), (2, 3)]
+    eliminate = distances.Distance.generate_tuple_sections([1,1,1,2,3])
+    np.testing.assert_array_equal(ans, eliminate)
+    ans = [(3, 4), (3, 5), (4, 5)]
+    eliminate = distances.Distance.generate_tuple_sections([3,3,4,4,5,5])
+    np.testing.assert_array_equal(ans, eliminate)
+    distances.Distance.api_calls = {(1,2): 1.0, (1,3):2.0}
+    ans = [2,3]
+    actual = distances.Distance.eliminate_sections([1,1,1,2,3])
+    np.testing.assert_array_equal(ans, actual)
+    
+if __name__ == "__main__":
+    test_generateScheduleCombinations()
+    test_eliminate_sections()
+    print("Everything passed")
