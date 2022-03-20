3515ce4dd6eabfeed6ec64535ffcc13e9e6e7f46
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2022-03-20 00:50:22 -0700

    Unit tests, add tests for new Course func

--- web_scraping/test.py
+++ web_scraping/test.py
@@ -1,55 +1,63 @@
 from course import Course
+import unittest
 
-def test_course():
-  math241 = Course("spring", "2022", "MATH241")
-  try:
-      math241.subject == "MATH"
-  except:
-      print("bad subject")
-      raise ValueError
-            
-  try:
-      math241.num == "241"
-  except:
-      print("bad num")
-      raise ValueError
+class TestWebScraping(unittest.TestCase):
   
-  try:
-      math241.sections['ADB']
-  except:
-      print("bad sections")
-      raise ValueError
-  
-  test_section(math241.sections['ADB'])
+  def test_course_accessors_init(self):
+    math241 = Course("spring", "2022", "MATH241")
+    self.assertEqual(math241.get_subject(), "MATH")
+    self.assertEqual(math241.get_number(), "241")
+    self.assertEqual(math241.get_section('ADB').get_course(), "MATH241")
 
-def test_section(s):
-  try:
-    s.course == "MATH"
-  except:
-    print("bad subject")
-    raise ValueError
-            
-  try:
-    s.name == "ADB"
-  except:
-    print("bad name")
-    raise ValueError
+    # aas100 = Course("spring", "2022", "AAS100")
+    # self.assertEqual(aas100.get_section('AB ').get_course(), "AAS100")
+
+  def test_split_sections(self):
+    aas100 = Course("spring", "2022", "AAS100")
+    sections_split = aas100.split_sections_on_type()
+    self.assertEqual(sections_split["Lecture-Discussion"][0].get_name(), "AB")
   
-  try:
-    s.section_type == "Discussion/Recitation"
-  except:
-    print("bad type")
-    raise ValueError
-            
-  try:
-    s.section_type == "Altgeld Hall"
-  except:
-    print("bad location")
-    raise ValueError
-  try:
-    s.days == "TR"
-  except:
-    print("bad days")
-    raise ValueError
+  def test_section_accessors_init(self):
+    section = Course("spring", "2022", "MATH241").sections['ADB']
+    self.assertEqual(section.get_course(), "MATH241")
+    self.assertEqual(section.get_name(), "ADB")
+    self.assertEqual(section.get_type(), "Discussion/Recitation")
+    self.assertEqual(section.get_location(), "Altgeld Hall")
+    self.assertEqual(section.get_days(), "TR")
+  
+  def test_section_time_conflict(self):
+    math241 = Course("spring", "2022", "MATH241")
+    aas100 = Course("spring", "2022", "AAS100")
+    self.assertTrue(math241.get_section('ADB').has_time_conflict(aas100.get_section("AB")))
+  
+  def test_course_time_confict(self):
+    math241 = Course("spring", "2022", "MATH241")
+    aas100 = Course("spring", "2022", "AAS100")
+    section_ls = [math241.get_section('ADB'), aas100.get_section('AB')]
+    self.assertTrue(math241.has_time_conflict(section_ls))
+  
+  # TODO: use actual asserts in this test
+  def test_linked_sections_simple(self):
+    aas297 = Course("spring", "2022", "AAS297")
+
+    linked_section_ls = aas297.get_linked_sections()
+    for linked in linked_section_ls:
+      for section in linked:
+        print(section.get_name())
+      print('\n')
+  
+  # TODO: use actual asserts in this test
+  def test_linked_sections_complex(self):
+    aas297 = Course("spring", "2022", "MATH241")
+
+    linked_section_ls = aas297.get_linked_sections()
+    for linked in linked_section_ls:
+      for section in linked:
+        print(section.get_name())
+      print('\n')
+
+
+
    
-test_course()
+if __name__ == '__main__':
+    unittest.main()
