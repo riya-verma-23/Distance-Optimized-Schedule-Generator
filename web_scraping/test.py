d803c19b07f1e6d2b438536d14cd546473c96493
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2022-03-20 02:18:17 -0700

    Test equals, update linked section tests, etc
    also:
    - linked section tests now use asserts instead of printing
    - fixed import error
    - removed TODO comments

--- web_scraping/test.py
+++ web_scraping/test.py
@@ -1,4 +1,5 @@
 from course import Course
+from section import Section
 import unittest
 
 class TestWebScraping(unittest.TestCase):
@@ -8,9 +9,6 @@ class TestWebScraping(unittest.TestCase):
     self.assertEqual(math241.get_subject(), "MATH")
     self.assertEqual(math241.get_number(), "241")
     self.assertEqual(math241.get_section('ADB').get_course(), "MATH241")
-
-    # aas100 = Course("spring", "2022", "AAS100")
-    # self.assertEqual(aas100.get_section('AB ').get_course(), "AAS100")
 
   def test_split_sections(self):
     aas100 = Course("spring", "2022", "AAS100")
@@ -25,6 +23,15 @@ class TestWebScraping(unittest.TestCase):
     self.assertEqual(section.get_location(), "Altgeld Hall")
     self.assertEqual(section.get_days(), "TR")
   
+  def test_course_equals(self):
+    math241_1 = Course("spring", "2022", "MATH241")
+    math241 = Course("spring", "2022", "MATH241")
+    self.assertTrue(math241_1 == math241)
+  
+  def test_section_equals(self):
+    math241 = Course("spring", "2022", "MATH241")
+    self.assertTrue(math241.get_section('ADB') == math241.get_section('ADB'))
+  
   def test_section_time_conflict(self):
     math241 = Course("spring", "2022", "MATH241")
     aas100 = Course("spring", "2022", "AAS100")
@@ -36,28 +43,41 @@ class TestWebScraping(unittest.TestCase):
     section_ls = [math241.get_section('ADB'), aas100.get_section('AB')]
     self.assertTrue(math241.has_time_conflict(section_ls))
   
-  # TODO: use actual asserts in this test
+  
   def test_linked_sections_simple(self):
     aas297 = Course("spring", "2022", "AAS297")
 
     linked_section_ls = aas297.get_linked_sections()
-    for linked in linked_section_ls:
-      for section in linked:
-        print(section.get_name())
-      print('\n')
+    section_a = Section('A', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/297/62166.xml', aas297)
+    section_b = Section('B', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/297/73220.xml', aas297)
+
+    ans = [Course.LinkedSection([section_a]), Course.LinkedSection([section_b])]
+
+    for i in range(len(linked_section_ls)):
+      self.assertEqual(linked_section_ls[i], ans[i])
   
-  # TODO: use actual asserts in this test
+
   def test_linked_sections_complex(self):
-    aas297 = Course("spring", "2022", "MATH241")
+    math241 = Course("spring", "2022", "MATH241")
 
-    linked_section_ls = aas297.get_linked_sections()
-    for linked in linked_section_ls:
-      for section in linked:
-        print(section.get_name())
-      print('\n')
+    linked_section_ls = math241.get_linked_sections()
+    ada = Section('ADA', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46053.xml', math241)
+    adb = Section('ADB', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46054.xml', math241)
+    al1 = Section('AL1', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46060.xml', math241)
+    al2 = Section('AL2', 'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46067.xml', math241)
 
+    ans = [Course.LinkedSection([ada, al1]), Course.LinkedSection([ada, al2]), Course.LinkedSection([adb, al1]), Course.LinkedSection([adb, al2])]
 
-
+    for i in range(4):
+      self.assertEqual(linked_section_ls[i], ans[i])
+        
+  
+  def test_linked_section_equals(self):
+    linked_section = Course.LinkedSection(['A'])
+    linked_section1 = Course.LinkedSection(['A'])
+    self.assertTrue(linked_section1 == linked_section)
+    linked_section2 = Course.LinkedSection(['AB'])
+    self.assertFalse(linked_section2 == linked_section)
    
 if __name__ == '__main__':
     unittest.main()
