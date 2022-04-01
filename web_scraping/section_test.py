08df4e58cca24b3f0d0e76f9448c7284714a46f9
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2022-04-01 16:10:50 -0700

    Optimize section_test.py

--- web_scraping/section_test.py
+++ web_scraping/section_test.py
@@ -4,12 +4,12 @@ import unittest
 
 class TestSection(unittest.TestCase):
   
-  def test_bad_section_input(self):
-    math231 = Course("spring", "2022", "MATH231")
-    self.assertRaises(KeyError, math231.get_section, "ADZ")
-  
   def test_section_init_and_accessors(self):
-    section = Course("spring", "2022", "MATH241").sections['ADB']
+
+    section = Section('ADB',
+                      'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46054.xml',
+                      'MATH241') 
+
     self.assertEqual(section.get_course(), "MATH241")
     self.assertEqual(section.get_name(), "ADB")
     self.assertEqual(section.get_type(), "Discussion/Recitation")
@@ -19,35 +19,59 @@ class TestSection(unittest.TestCase):
     self.assertEqual(section.get_end().strftime("%H:%M"), "09:50")
 
   def test_section_equals(self):
-    aas297 = Course("spring", "2022", "AAS297")
-    section_a = aas297.get_section('A')
+    section_a = Section('A',
+                        'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/297/62166.xml',
+                        'AAS297')
+
+    section_b = Section('B',
+                            'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/297/73220.xml',
+                            'AAS297')
 
     self.assertEqual(section_a, section_a)
 
-    self.assertNotEqual(aas297.get_section('B'), section_a)
+    self.assertNotEqual(section_b, section_a)
 
-    aas297_1 = Course("fall", "2021", "AAS297")
-    self.assertNotEqual(aas297_1.get_section('A'), section_a)
+    section_a_1 = Section('A',
+                        'https://courses.illinois.edu/cisapp/explorer/schedule/2021/fall/AAS/297/64421.xml',
+                        'AAS297')
+    self.assertNotEqual(section_a_1, section_a)
     
   
   def test_section_time_conflict(self):
-    math241 = Course("spring", "2022", "MATH241")
-    aas100 = Course("spring", "2022", "AAS100")
 
-    aas100_ab = aas100.get_section("AB")
-    math241_ada = math241.get_section('ADA')
+    math241_ada = Section('ADA',
+                  'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46053.xml',
+                  'MATH241')
+    math241_adb = Section('ADB',
+                  'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46054.xml',
+                  'MATH241') 
+    math241_adc = Section('ADC',
+                  'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46056.xml',
+                  'MATH241')
+    math241_adf = Section('ADF',
+                  "https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/48355.xml",
+                  'MATH241')
+    math241_al1 = Section('AL1',
+                  "https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/MATH/241/46060.xml",
+                  'MATH241')
+    aas100_ab = Section('AB',
+                'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/100/69781.xml',
+                'AAS100') 
+    aas100_ad2 = Section('AD2',
+                "https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/100/41729.xml",
+                'AAS100')
 
     # start at same time
-    self.assertTrue(math241.get_section('ADB').has_time_conflict(aas100_ab))
+    self.assertTrue(math241_adb.has_time_conflict(aas100_ab))
 
     # start during second section
-    self.assertTrue(math241.get_section('ADC').has_time_conflict(aas100_ab))
+    self.assertTrue(math241_adc.has_time_conflict(aas100_ab))
     
     # start and end at same time, overlap in days
-    self.assertTrue(math241.get_section('AL1').has_time_conflict(aas100.get_section("AD2")))
+    self.assertTrue(math241_al1.has_time_conflict(aas100_ad2))
     
     # after section section
-    self.assertFalse(math241.get_section('ADF').has_time_conflict(aas100_ab))
+    self.assertFalse(math241_adf.has_time_conflict(aas100_ab))
 
     # before second section
     self.assertFalse(math241_ada.has_time_conflict(aas100_ab))
@@ -56,10 +80,11 @@ class TestSection(unittest.TestCase):
     self.assertFalse(math241_ada.has_time_conflict(math241_ada))
   
   def test_hash(self):
-    aas297 = Course("spring", "2022", "AAS297")
     dictionary = {}
     try:
-      section_a = aas297.get_section('A')
+      section_a = Section('A',
+                        'https://courses.illinois.edu/cisapp/explorer/schedule/2022/spring/AAS/297/62166.xml',
+                        'AAS297')
       dictionary[section_a] = "hello world"
     except:
       self.fail("hash failed unexpectedly!")
