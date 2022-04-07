9f069ba98cfe425be913490711298f4d2a8950c5
Irina Malyugina <56461226+IrinaM21@users.noreply.github.com>
2022-04-06 22:55:02 -0700

    Throw error if no XML file load

--- web_scraping/section.py
+++ web_scraping/section.py
@@ -44,7 +44,10 @@ class Section:
   # Initialize location, section type, meeting days, and start and end time
   # from Section XML file
   def init_location(self, section_path):
-    soup = BeautifulSoup(requests.get(section_path).text, "lxml-xml")
+    try:
+      soup = BeautifulSoup(requests.get(section_path).text, "lxml-xml")
+    except:
+      raise ValueError("timeout")
     self.section_type = soup.findAll("type")[0].string
 
     loc = soup.findAll("buildingName")
